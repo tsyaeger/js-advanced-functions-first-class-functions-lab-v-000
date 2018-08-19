428b173d036dc225c7c4734f79e88bbbee7396ca
taryn yaeger <tsyaeger@gmail.com>
2018-08-19 20:54:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers) {
+  const two = (function (drivers)
+}
+
+
+
+
+
+  describe('returnFirstTwoDrivers()', function () {
+    it('should return a new array containing the first two drivers from the passed-in array', function () {
+      expect(returnFirstTwoDrivers(drivers)).to.eql(['Sally', 'Bob']);
+    });
+
+    it('should be assigned to a constant', function () {
+      expect(function () { returnFirstTwoDrivers = 'testing reassignment'; }).to.throw(TypeError);
+    });
+  });
