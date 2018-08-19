1e92b06832d1c1031f2e87ca5e693853aa9e3f65
taryn yaeger <tsyaeger@gmail.com>
2018-08-19 22:20:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,3 @@ const returnFirstTwoDrivers = function two (drivers) {
 const returnLastTwoDrivers = function last_two(drivers) {
   return drivers.slice(-3)
 }
-
-describe('returnLastTwoDrivers()', function () {
-    it('should return an array of the last two drivers', function () {
-      expect(returnLastTwoDrivers(['Sally', 'Bob', 'Freddy', 'Claudia'])).to.eql(['Freddy', 'Claudia']);
-    });
-
-    it('should be assigned to a constant', function () {
-      expect(function () { returnLastTwoDrivers = 'testing reassignment'; }).to.throw(TypeError);
-    });
-  });
