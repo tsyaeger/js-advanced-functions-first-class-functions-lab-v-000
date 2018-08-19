574d9a30b275cd283fafb49ed88839456badd1d1
taryn yaeger <tsyaeger@gmail.com>
2018-08-19 22:21:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ const returnFirstTwoDrivers = function two (drivers) {
 const returnLastTwoDrivers = function last_two(drivers) {
   return drivers.slice(-2)
 };
+
+
+const selectingDrivers = function 
+
+escribe('selectingDrivers', function () {
+    it('has the `returnFirstTwoDrivers` function to as its first element', function () {
+      expect(selectingDrivers[0]).to.eql(returnFirstTwoDrivers);
+    });
+
+    it('has the `returnLastTwoDrivers` function to as its last element', function () {
+      expect(selectingDrivers[1]).to.eql(returnLastTwoDrivers);
+    });
+
+    it('allows us to invoke either function from the array', function () {
+      expect(selectingDrivers[0](drivers)).to.eql(['Sally', 'Bob']);
+
+      expect(selectingDrivers[1](drivers)).to.eql(['Freddy', 'Claudia']);
+    });
+  });
