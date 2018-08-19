886466ef33c3050516cd9116d0028230425a93cc
taryn yaeger <tsyaeger@gmail.com>
2018-08-19 22:03:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 
 const returnFirstTwoDrivers = function returnFirstTwoDrivers(drivers) {
   let firstTwo = [];
-  firstTwo.push(firstTwo.slice(0,1));
+  firstTwo.push(firstTwo.slice(0,2));
   return firstTwo;
 };
