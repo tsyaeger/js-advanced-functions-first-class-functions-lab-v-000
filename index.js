2800c0c401f4c1217af28d963ff62048dc6a53b5
taryn yaeger <tsyaeger@gmail.com>
2018-08-19 23:02:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,21 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler= createFareMultiplier(3);
 
+
+const selectDifferentDrivers(arr,fn) {
+  return fn(arr);
+}
+
+describe('selectDifferentDrivers(arrayOfDrivers, function)', function () {
+    it('returns the first two drivers when passed returnFirstTwoDrivers() as the second argument', function () {
+      expect(selectDifferentDrivers(drivers, returnFirstTwoDrivers)).to.eql(['Sally', 'Bob']);
+    });
+
+    it('returns the last two drivers when passed returnLastTwoDrivers() as the second argument', function () {
+      expect(selectDifferentDrivers(drivers, returnLastTwoDrivers)).to.eql(['Freddy', 'Claudia']);
+    });
+  });
+  
+  
+  
+  
